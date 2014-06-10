98efb63c3212d36358774709f633cf87e773d628
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-06-10 10:34:18 +0000

    Use commonjs Note: tests have not been configured

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-(function($) {
-  'use strict';
+'use strict';
+var $ = require('jquery');
 
-  function resize(element, container) {
-    element.height(container.height());
-  }
+function resize(element, container) {
+  element.height(container.height());
+}
 
-  function fullHeight(element, options) {
-    var container = $(options.container || window);
+function fullHeight(element, options) {
+  var container = $(options.container || window);
 
-    container.on('resize.aranja', function() {
-      resize(element, container);
-    });
-
+  container.on('resize.aranja', function() {
     resize(element, container);
-  }
-
-  // jQuery plugin
-  $.fn.fullHeight = function(config) {
-    config = config || {};
-    $.extend(true, config, this.data());
-    fullHeight(this, config);
-    return this;
-  };
-
-  // Data attributes
-  $(function() {
-    $('[data-full-height]').fullHeight();
   });
-})(jQuery);
+
+  resize(element, container);
+}
+
+// jQuery plugin
+$.fn.fullHeight = function(config) {
+  config = config || {};
+  $.extend(true, config, this.data());
+  fullHeight(this, config);
+  return this;
+};
+
+// Data attributes
+$(function() {
+  $('[data-full-height]').fullHeight();
+});
