763f89664fcf59bfe606c2f7a012a22f0c3c7783
Eirikur Nilsson <eirikur@nilsson.is>
2014-07-23 05:10:40 +0000

    Support offset and property options. Clean up.

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 'use strict';
 var $ = require('jquery');
 
-function resize(element, container) {
-  element.height(container.height());
+/**
+ * Makes element the same size as another element, usually a viewport like window.
+ * Updates when window is resized.
+ * @param el - the element to resize
+ * @param options.container - the element to resize based on. Defaults to `window`.
+ * @param options.offset - number of pixels to subtract size.
+ * @param options.property - which property to set when resizing. Defaults to `height` while `min-height`
+ *                           and `max-height` can also be useful.
+ * @constructor
+ */
+function ViewportHeight(el, options) {
+  this.el = $(el);
+
+  options = $.extend({}, ViewportHeight.DEFAULTS, options, this.el.data());
+  this.container = $(options.container || window);
+  this.offset = parseInt((options.offset || '0'), 10);
+  this.property = options.property || 'height';
+
+  this.resize();
+  this.container.on('resize.aranja.viewportheight', $.proxy(function() {
+    this.resize();
+  }, this));
 }
 
-function fullHeight(element, options) {
-  var container = $(options.container || window);
+ViewportHeight.prototype.resize = function() {
+  var newHeight = this.container.height() - this.offset;
+  this.el.css(this.property, newHeight);
+};
 
-  container.on('resize.aranja', function() {
-    resize(element, container);
-  });
+ViewportHeight.prototype.dispose = function() {
+  this.container.off('resize.aranja.viewportheight');
+};
 
-  resize(element, container);
-}
 
 // jQuery plugin
-$.fn.fullHeight = function(config) {
-  config = config || {};
-  $.extend(true, config, this.data());
-  fullHeight(this, config);
-  return this;
+$.fn.fullHeight = function(options) {
+  return this.each(function() {
+    var el = $(this);
+    var data = el.data('aranja.viewportheight');
+    var opts = typeof options === 'object' && options;
+
+    if (!data) { el.data('aranja.viewportheight', (data = new ViewportHeight(el, opts))); }
+    if (typeof options == 'string') { data[options](); }
+  });
 };
+
 
 // Data attributes
 $(function() {
