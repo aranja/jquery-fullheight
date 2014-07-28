794b43d2c3a7d5ad43d213997359d6f714c1fc16
Eirikur Nilsson <eirikur@nilsson.is>
2014-07-28 03:44:40 +0000

    Fix for Chrome for Android
    Chrome for Android resizes the browser a lot when scrolling due to the
    address bar collapsing. This causes a lot of arbitrary layout jumps and
    slow image resize operations with this plugin.
    
    So for device UA where height should not change, we only update if the
    width changes as well (f.ex. orientation changes).

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 var $ = require('jquery');
 
+// Device check for limiting resize handling.
+var IS_DEVICE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 /**
  * Makes element the same size as another element, usually a viewport like window.
  * Updates when window is resized.
@@ -15,17 +18,48 @@ function ViewportHeight(el, options) {
   this.el = $(el);
 
   options = $.extend({}, ViewportHeight.DEFAULTS, options, this.el.data());
-  this.container = $(options.container || window);
-  this.offset = parseInt((options.offset || '0'), 10);
-  this.property = options.property || 'height';
+  this.container = $(options.container);
+  this.offset = parseInt((options.offset), 10);
+  this.property = options.property;
 
-  this.resize();
-  this.container.on('resize.aranja.viewportheight', $.proxy(function() {
-    this.resize();
-  }, this));
+  // Chrome for Android resizes the browser a lot when scrolling due to the address bar collapsing.
+  // This causes a lot of arbitrary layout jumps and slow image resize operations with this plugin.
+  // So for device UA where height should not change, we only update if the width changes as well (f.ex.
+  // orientation changes).
+  this.allowDeviceHeightResize = !(options.allowDeviceHeightResize == null || options.allowDeviceHeightResize === false || options.allowDeviceHeightResize === 'false');
+  this.lastWidth = this.container.width();
+
+  // Watch for screen resize.
+  this.container.on('resize.aranja.viewportheight', $.proxy(this, '_onResize'));
+
+  // Apply the height.
+  this.update();
 }
 
-ViewportHeight.prototype.resize = function() {
+/**
+ * Default Options
+ * @type {Object}
+ */
+ViewportHeight.DEFAULTS = {
+  allowDeviceHeightResize: false,
+  container: window,
+  offset: 0,
+  property: 'height'
+};
+
+ViewportHeight.prototype._onResize = function() {
+  var newWidth = this.container.width();
+  var allowResize = !IS_DEVICE || this.allowDeviceHeightResize || newWidth !== this.lastWidth;
+
+  // Do the update if expected.
+  if (allowResize) {
+    this.update();
+  }
+
+  this.lastWidth = newWidth;
+};
+
+ViewportHeight.prototype.update = function() {
   var newHeight = this.container.height() - this.offset;
   this.el.css(this.property, newHeight);
 };
